9d07af10a6eaa36cd104b02db14ae85822210834
elijah <cuiyeqing960904@gmail.com>
2025-12-10 11:43:41 +0300

    context

--- examples/weather/main.py
+++ examples/weather/main.py
@@ -1,27 +1,68 @@
 import requests
 from dotenv import load_dotenv
 from langchain.agents import create_agent
+from langchain.chat_models import init_chat_model
 from langchain.tools import tool
+from dataclasses import dataclass
+
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.prebuilt import ToolRuntime
+from langgraph_sdk.schema import Context
+from requests_toolbelt import user_agent
 
 load_dotenv()
+
+@dataclass
+class Context:
+    user_id: str
+
+@dataclass
+class ResponseFormat:
+    summary: str
+    temperature_celsius: float
+    temperature_fahrenheit: float
+    humidity: float
+
 
 @tool('get_weather', description="return weather information for a given city", return_direct=True)
 def get_weather(city: str):
     response = requests.get(f'https://wttr.in/{city}?format=j1')
     return response.json()
 
+@tool('locate_user', description="Look up a user's city based on the context")
+def locate_user(runtime: ToolRuntime[Context]):
+    match runtime.context.user_id:
+        case 'ABC123':
+            return 'Vienna'
+        case 'XYZ456':
+            return 'london'
+        case _:
+            return 'Unknown'
+
+model = init_chat_model('gpt-4.1', temperature=0.3)
+
+checkpointer = InMemorySaver()
+
 agent = create_agent(
-    model="gpt-4.1",
-    tools=[get_weather],
-    system_prompt='You are a helpful weather assistant, who always makes jokes'
+    model=model,
+    tools=[get_weather, locate_user],
+    system_prompt='You are a helpful weather assistant, who always makes jokes',
+    context_schema=Context,
+    response_format=ResponseFormat,
+    checkpointer=checkpointer
 )
 
-response = agent.invoke({
-    'messages': [
-        {'role': 'user', 'content': "What is the weather in london?"}
-    ]
-})
+config = {'configurable': {'thread_id': 1}}
+
+response = agent.invoke(
+    {
+        'messages': [
+            {'role': 'user', 'content': "What is the weather in london?"}
+        ]
+    },
+    config=config,
+    context=Context(user_id='ABC123')
+)
 
 
 print(response)
-print(response['messages'][-1].content)
